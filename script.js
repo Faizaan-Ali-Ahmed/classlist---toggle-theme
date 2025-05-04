eaf20db2f0ac3ffca7bcdba93bee30575fd2317e
Faizaan-Ali-Ahmed <faizaanali553@gmail.com>
2025-05-04 16:59:41 +0530

    done

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ let moonLogo = document.querySelector('.ri-moon-clear-line')
 let day = document.querySelector('.lay1')
 let night = document.querySelector('.lay2')
 
-let mark = document.querySelector('.mark')
+let footMark = document.querySelector('.mark')
 
-sunBtn.addEventListener('click',function(){
 
+function lightTheme(){
     day.style.opacity = 1
 
     h1.style.color = 'red'
@@ -22,7 +22,7 @@ sunBtn.addEventListener('click',function(){
     sunBtn.style.backgroundColor = ' rgba(250, 206, 121, 0.8)'
     sunBtn.style.border = '2px solid rgb(241, 178, 4)'
     foot.style.backgroundColor = 'rgba(241, 110, 110, 0.9)'
-    mark.style.color = 'red'
+    footMark.style.color = 'red'
     
     moonLogo.style.color = 'silver'
     moonBtn.style.backgroundColor = 'transparent'
@@ -32,30 +32,53 @@ sunBtn.addEventListener('click',function(){
         day.style.opacity = 0
 
     }, 1000);
+    }
 
+    function darkTheme(){
+        night.style.opacity = 1
+    
+        h1.style.color = 'black'
+        h4.style.color = 'black'
+        main.style.backgroundColor = ' rgb(49, 47, 47)'
+        moonBtn.style.backgroundColor = 'grey'
+        moonLogo.style.color = 'white'
+        moonBtn.style.border = '2px solid white'
+        foot.style.backgroundColor = 'rgb(99, 95, 95)'
+        footMark.style.color = 'white'
+    
+        sunBtn.style.backgroundColor = ' transparent'
+        sunBtn.style.border = 'none'
+    
+        setTimeout(() => {
+            night.style.opacity = 0
+    
+        }, 1000);
+        
+        }
+
+sunBtn.addEventListener('click',function(){
+
+    lightTheme()
+    localStorage.setItem('theme','lightTheme')
 
 })
 
 
-
 moonBtn.addEventListener('click', function(){
-    night.style.opacity = 1
 
-    h1.style.color = 'black'
-    h4.style.color = 'black'
-    main.style.backgroundColor = ' rgb(49, 47, 47)'
-    moonBtn.style.backgroundColor = 'grey'
-    moonLogo.style.color = 'white'
-    moonBtn.style.border = '2px solid white'
-    foot.style.backgroundColor = 'rgb(99, 95, 95)'
-    mark.style.color = 'white'
+    darkTheme()
+    localStorage.setItem('theme','darkTheme')
+   
+})
 
-    sunBtn.style.backgroundColor = ' transparent'
-    sunBtn.style.border = 'none'
+// page reload theme check 
+window.addEventListener('load',function(){
 
-    setTimeout(() => {
-        night.style.opacity = 0
-
-    }, 1000);
+    document.body.classList.add('loaded')
+    let savedTheme = localStorage.getItem('theme')
+    if(savedTheme === 'lightTheme')  
+        lightTheme()
     
+    else if(savedTheme === 'darkTheme')
+        darkTheme()
 })
